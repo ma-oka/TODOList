b0007a5a1963e473b6169e9505bc4455f0ee7b19
Masatoshi Oka <satoshing236236@gmail.com>
2018-12-27 18:39:06 +0900

    修正

--- conf/evolutions/default/1.sql
+++ conf/evolutions/default/1.sql
@@ -3,7 +3,10 @@
 # --- !Ups
 
 CREATE SEQUENCE task_id_seq;
-create table task(id integer NOT NULL DEFAULT nextval('task_id_seq'),label varchar(255));
+create table task(
+  id integer NOT NULL DEFAULT nextval('task_id_seq'),
+  label varchar(255)
+);
 
 # --- !Downs
 
